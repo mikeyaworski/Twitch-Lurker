ce947578dda183843182a6989b287ede511e924c
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2023-10-15 22:24:29 -0400

    Adds SmallRadio widget with 7px padding

---
+++ app/src/widgets/SmallRadio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  withStyles,
+  Theme,
+} from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+
+export default withStyles((theme: Theme) => ({
+  root: {
+    padding: 7,
+  },
+}))(Radio);
