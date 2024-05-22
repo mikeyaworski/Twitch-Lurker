eca7c0cf260099aed5ecc10deb87a4b8429a84aa
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2024-05-22 01:17:49 -0400

    Upgrades @crxjs/vite-plugin to v2 beta Removes hacky middleware in Vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,18 +11,6 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'build');
 const publicDir = resolve(__dirname, 'public');
 
-// Using this hack workaround until the @crxjs/vite-plugin supports Vite v5
-// @ts-ignore
-const viteManifestHackIssue846: Plugin & { renderCrxManifest: (manifest: any, bundle: any) => void } = {
-  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-  name: 'manifestHackIssue846',
-  renderCrxManifest(_manifest, bundle) {
-    bundle['manifest.json'] = bundle['.vite/manifest.json'];
-    bundle['manifest.json'].fileName = 'manifest.json';
-    delete bundle['.vite/manifest.json'];
-  },
-};
-
 export default defineConfig({
   resolve: {
     alias: {
@@ -31,7 +19,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    viteManifestHackIssue846,
     crx({
       manifest: manifest as ManifestV3Export,
       contentScripts: {
