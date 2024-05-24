70643e23bf5341dba59db5a069ffe47fa2e18c24
DESKTOP-V3FPRIF\mikeyaworski <mikeyaworski@hotmail.com>
2024-05-24 02:09:05 -0400

    Alters manifest for Firefox depending on TARGET environment variable

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,13 +3,24 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { crx, ManifestV3Export } from '@crxjs/vite-plugin';
 
-import manifest from './manifest.json';
+import rawManifest from './manifest.json';
 
 const isDev = process.env.ENVIRONMENT === 'DEVELOPMENT';
+const isFirefox = process.env.TARGET === 'FIREFOX';
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'build');
 const publicDir = resolve(__dirname, 'public');
+
+function getManifest(): ManifestV3Export {
+  return {
+    ...rawManifest,
+    background: isFirefox ? {
+      scripts: [rawManifest.background.service_worker],
+      type: 'module',
+    } : rawManifest.background,
+  };
+}
 
 export default defineConfig({
   resolve: {
@@ -20,7 +31,7 @@ export default defineConfig({
   plugins: [
     react(),
     crx({
-      manifest: manifest as ManifestV3Export,
+      manifest: getManifest(),
       contentScripts: {
         injectCss: true,
       },
