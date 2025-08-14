09892f450605bd1b901bea545196918f4db68775
Michael Yaworski <mikeyaworski@hotmail.com>
2025-08-13 20:07:51 -0400

    Fix(Kick): On error, log and continue if one channel fails
    If a single channel fails to fetch for whatever reason
    (e.g. channel was deleted), this logs the error and continues
    with the other channels. Prior to this, it would throw an error
    and not return any channel data.

--- app/src/service-worker/polling/kick.ts
+++ app/src/service-worker/polling/kick.ts
@@ -1,31 +1,40 @@
 import {
   KICK_API_BASE,
 } from 'src/app-constants';
+import { error } from 'src/logging';
 import { ChannelType, KickChannel } from 'src/types';
+import { notEmpty } from 'src/utils';
 
 const getApi = () => async (route: string, params: [string, string | number][] = []) => {
-  const url = new URL(`${KICK_API_BASE}${route}`);
+  const href = `${KICK_API_BASE}${route}`;
+  const url = new URL(href);
   params.forEach(([key, value]) => {
     if (value) url.searchParams.append(key, String(value));
   });
   const res = await fetch(url.href, {
     method: 'GET',
   });
+  if (res.status >= 400) throw new Error(`Error fetching ${href}: ${res.status} ${res.statusText}`);
   const json = await res.json();
-  if (res.status >= 400) throw json;
   return json;
 };
 
 function fetchArbitraryChannels(usernames: string[]) {
-  return Promise.all(usernames.map(username => {
-    return getApi()(`/channels/${username}`);
+  return Promise.all(usernames.map(async username => {
+    try {
+      const data = await getApi()(`/channels/${username}`);
+      return data;
+    } catch (err) {
+      error(err);
+      return null;
+    }
   }));
 }
 
 export async function fetchData(addedChanels: string[]): Promise<KickChannel[]> {
   const channels = await fetchArbitraryChannels(addedChanels);
 
-  return channels.map(channelData => {
+  return channels.filter(notEmpty).map(channelData => {
     const stream = channelData.livestream;
     const channel: KickChannel = {
       type: ChannelType.KICK,
